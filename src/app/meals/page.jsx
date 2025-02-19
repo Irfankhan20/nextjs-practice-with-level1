ad9fe9e2e85d143474edbc82eeab49fdfbf25576
Irfankhan20 <sipathan96@gmail.com>
2025-02-19 11:21:56 +0600

    font and image optimization

--- src/app/meals/page.jsx
+++ src/app/meals/page.jsx
@@ -1,10 +1,11 @@
+import Image from "next/image";
 import MealSearchInput from "./components/MealSearchInput";
 
 export const metadata = {
   title: "All Meals",
-
   description: "Meals loaded from MealDB API",
 };
+
 const MealsPage = async ({ searchParams }) => {
   const query = (await searchParams) || "";
 
@@ -15,17 +16,18 @@ const MealsPage = async ({ searchParams }) => {
       );
       const data = await res.json();
 
-      return data.meals;
+      return data.meals || [];
     } catch (error) {
-      console.log(error);
+      console.error("Error fetching meals:", error);
       return [];
     }
   };
+
   const mealss = (await fetchMeals()) || [];
 
   return (
     <div>
-      <MealSearchInput></MealSearchInput>
+      <MealSearchInput />
       <p>Total Meals: {mealss?.length}</p>
 
       <div className="grid grid-cols-4 gap-4">
@@ -34,6 +36,16 @@ const MealsPage = async ({ searchParams }) => {
             className="border-2 p-3 shadow-xl rounded-xl border-purple-600"
             key={meal?.idMeal}
           >
+            {meal.strMealThumb ? (
+              <Image
+                width={641}
+                height={641}
+                src={meal?.strMealThumb}
+                alt={meal?.strMeal || "Meal Image"}
+              />
+            ) : (
+              <p>No Image Available</p>
+            )}
             <p>Meal Name: {meal?.strMeal}</p>
             <p>Meal Details: {meal?.strInstructions}</p>
           </div>
